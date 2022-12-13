984e33f70f791a000efce18f72b15624fd091b0c
Grayson Smith <graysonsmith91@gmail.com>
2022-12-13 09:13:16 -0600

    initial commit

---
+++ scripts/Metals.js
@@ -0,0 +1,31 @@
+import { getMetals, setMetal } from "./database.js"
+
+const metals = getMetals()
+
+document.addEventListener(
+    "change",
+    (event) => {
+        if (event.target.name === "metal") {
+            setMetal(parseInt(event.target.value))
+        }
+    }
+)
+
+export const Metals = () => {
+    let html = "<ul>"
+
+    // This is how you have been converting objects to <li> elements
+    // I changed it to map from for loop
+
+    const listItems = metals.map(metal => {
+        return `<li>
+            <input type="radio" name="metal" value="${metal.id}" /> ${metal.metal}
+        </li>`
+    })
+
+
+    html += listItems.join("")
+    html += "</ul>"
+    return html
+}
+
